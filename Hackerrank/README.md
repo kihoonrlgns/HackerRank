a8927bffd9ab594590f478e5632ce15549c79552
Kihoon Lee <36075511+kihoonrlgns@users.noreply.github.com>
2021-06-15 21:14:15 +0900

    Delete Hackerrank directory

--- Hackerrank/README.md
+++
@@ -1,2 +0,0 @@
-# sql-practice
-sql exercises, practice, solution
--- Hackerrank/Weather Observation Station 1.sql
+++
@@ -1,5 +0,0 @@
-#Query a list of CITY and STATE from the STATION table.
-#The STATION table is described as follows:
-
-SELECT city, state
-FROM station
